139582769eb1b92e7f23c332d4fa3e59c2641b10
AlinaZadorna <alinazrdn@gmail.com>
2022-10-06 20:24:10 +0300

    add asserts. Refactor class name

--- src/test/java/CartTests.java
+++ src/test/java/CartTests.java
@@ -1,10 +1,11 @@
+import com.codeborne.selenide.Condition;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class BaseTest {
+public class CartTests {
     @Test
     public void addToCart() {
         open("https://demo.nopcommerce.com/build-your-own-computer");
@@ -13,5 +14,8 @@ public class BaseTest {
         $(By.id("product_attribute_4_8")).click();
         $(By.id("add-to-cart-button-1")).click();
         $(By.xpath("//a[contains(text(),'shopping cart')]")).click();
+
+        $(By.xpath("(//div[contains(@class,'attributes')])[2]")).shouldHave(Condition.text("RAM: 8GB"));
+        $(By.xpath("(//div[contains(@class,'attributes')])[2]")).shouldHave(Condition.text("HDD: 320 GB"));
     }
 }
